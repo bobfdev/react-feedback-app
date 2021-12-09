7693bf7f81ec7b0c73b85fb7bea3694116bee309
Bob F <bobfdev@gmail.com>
2021-12-09 17:38:35 -0600

    updated files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
     return (
-        <h1>Hello</h1>
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackList feedback={feedback} />
+            </div>
+        </>
     )
 }
 
